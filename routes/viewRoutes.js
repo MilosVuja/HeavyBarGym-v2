dc7c54c8d1aec5bbe6897852261a1aba0fadfa93
Milos Vujicic <milos.vujicic.dev@gmail.com>
2024-04-24 01:17:39 +0200

    bzv

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,8 +4,6 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.isLoggedIn);
-
 router.get('/', authController.isLoggedIn, viewController.getMainPage);
 router.get('/login', authController.isLoggedIn, viewController.getLoginPage);
 router.get('/profile', authController.protect, viewController.getProfilePage)
